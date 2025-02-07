d93db704bc2a73bf2d521ec06303d13da4e653b4
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-02-07 18:55:30 -0500

    add faucet variable

---
+++ js/variables.js
@@ -0,0 +1,35 @@
+
+var faucet = L.geoJson(faucet, {
+    pointToLayer: function (feature, latlng) {
+        return L.circle(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#5a5247',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.informant}</b>
+					<br>Attic type: ${props.faucet}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+    }
+})
