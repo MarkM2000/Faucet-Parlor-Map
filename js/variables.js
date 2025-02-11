2a49f1942a3835c449b2193fe7a4324908e369b5
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-02-10 19:00:56 -0500

    add living room variable

--- js/variables.js
+++ js/variables.js
@@ -138,3 +138,40 @@ var tap = L.geoJson(tap, {
         });
     }
 })
+
+// Parlor Variables
+var living_room = L.geoJson(living_room, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#5a5247',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.informant}</b>
+                    <br>Project: ${props.project}</b>
+					<br>Attic type: ${props.living_room}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+    }
+})
