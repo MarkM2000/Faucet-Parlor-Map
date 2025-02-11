66edf8311d0fa833fcc47e1ac0f6fa6fc8ef605f
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-02-11 13:04:18 -0500

    add sitting room variable

--- js/variables.js
+++ js/variables.js
@@ -247,3 +247,39 @@ var setting_room = L.geoJson(setting_room, {
         });
     }
 })
+
+var sitting_room = L.geoJson(sitting_room, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#5a5247',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+                    <br>Project: ${props.project}</b>
+					<br>Attic type: ${props.living_room}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+    }
+})
