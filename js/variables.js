4a8b60f28f65bf8ead5db3c50bfc8434c5548cf2
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-02-15 13:03:00 -0500

    add hydrant variable

--- js/variables.js
+++ js/variables.js
@@ -34,6 +34,41 @@ var faucet = L.geoJson(faucet, {
     }
 })
 
+var hydrant = L.geoJson(hydrant, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#ff0d00',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.informant}</b>
+					<br>Attic type: ${props.faucet}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#ff0d00'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#ff0d00'
+            });
+        });
+    }
+})
+
 var spigot = L.geoJson(spigot, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
