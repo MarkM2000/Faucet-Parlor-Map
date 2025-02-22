e2aea0c338b94f3e68b7c817a36fc4af3eb9474b
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-02-21 20:14:24 -0500

    add corndodger variable

--- js/variables.js
+++ js/variables.js
@@ -428,6 +428,42 @@ var cornbread = L.geoJson(cornbread, {
     }
 })
 
+var corndodger = L.geoJson(corndodger, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#cfa600',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+                    <br>Project: ${props.Project}</b>
+					<br>Attic type: ${props.cornbread}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#cfa600'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#cfa600'
+            });
+        });
+    }
+})
+
 var cornpones = L.geoJson(cornpones, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
